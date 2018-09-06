e48ba000d428d36e97234010d93098e0b318d05c
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-06 11:38:10 -0500

    New url to get a community

--- routes/community.js
+++ routes/community.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const community = require('../controllers/community');
 
 // Route list
+router.get('/get/all', community.getAll);
 router.get('/get/:id', community.getCommunity);
 router.post('/add', community.addCommunity);
+router.put('/update', community.updateCommunity);
 
 module.exports = router;
