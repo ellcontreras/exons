bb48cbaae39394743624c5505a279a8c234c0bd2
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-06 08:25:01 -0500

    Create the navbar component to use in the layout

---
+++ front/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar is-link">
+    <div class="navbar-brand">
+      <h1 class="title is-1 has-text-white">
+        Comm
+      </h1>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+nav {
+  margin-bottom: 3em;
+}
+</style>
