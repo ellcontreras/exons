e1d277710c87dbf769d6aa9a0363476b846183a9
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-06 11:37:49 -0500

    New param to the Card component

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
     <div>
-        <h1 class="title is-1 has-text-center">
-            Hola
+        <h1 class="title is-1 has-text-centered">
+            Communities
         </h1>
-        <Card title="asdcadcadscac" description="asdchjsbdchjbasdcbhjasdbchjbdhjcdchb"></Card>
+        <Card v-for="community in communities" :key="community._id" 
+            :id="community._id"
+            :title="community.title" :description="community.description"/>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 import Card from '@/components/Card'
 
 export default {
     name: 'Home',
-    components: { Card }
+    components: { Card },
+    data() {
+        return {
+            communities: []
+        }
+    },
+    beforeMount() {
+        axios.get('http://localhost:8080/api/community/get/all').then(res => {
+            this.communities = res.data.communities;
+        }).catch(err => {
+            console.log(err);
+        });
+    }
 }
 </script>
 
