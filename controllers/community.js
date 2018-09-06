1f6b7d8e6b8da6affc178b665713c339188aebaf
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-06 11:36:48 -0500

    Link to the community view

--- controllers/community.js
+++ controllers/community.js
@@ -19,8 +19,8 @@ var getCommunity = (req, res) => {
 var addCommunity = (req, res) => {
     var community = new Community();
 
-    community.title = "first";
-    community.description = "lorem ipsum tu gfa";
+    community.title = req.body.title;
+    community.description = req.body.description;
 
     community.save((err, community_save) => {
         if (err) {
@@ -35,7 +35,39 @@ var addCommunity = (req, res) => {
     })
 };
 
+var updateCommunity = (req, res) => {
+    var body = req.body;
+
+    Community.findByIdAndUpdate(body._id, body, (err, community) => {
+        if (err) {
+            res.ststua(500).send({message: "Internal server error!"});
+        } else {
+            if (!communityUpdated) {
+                res.status(404).send({message: "The commnity has not been found!"});
+            } else {
+                res.status(200).send({community});
+            }
+        }
+    });
+};
+
+var getAll = (req, res) => {
+    Community.find((err, communities) => {
+        if (err) {
+            res.status(500).send({message: "Internal server errror"});
+        } else {
+            if (!communities) {
+                res.status(404).send({message: "There is not communitites"});
+            } else {
+                res.status(200).send({communities});
+            }
+        }
+    }).sort({_id:-1});
+}
+
 module.exports = {
     getCommunity,
-    addCommunity
+    addCommunity,
+    updateCommunity,
+    getAll
 };
--- front/src/components/Card.vue
+++ front/src/components/Card.vue
@@ -1,21 +1,30 @@
 <template>
-    <div class="card">
-        <div class="card-heade">
-            <div class="card-header-title">{{ title }}</div>
+    <router-link :to="`/community/${id}`">
+        <div class="card">
+            <div class="card-header">
+                <div class="card-header-title">{{ title }}</div>
+            </div>
+            <div class="card-content">
+                {{ description }}
+            </div>
         </div>
-        <div class="card-content">
-            {{ description }}
-        </div>
-    </div>
+    </router-link>
 </template>
 
 <script>
 export default {
     name: 'Card',
     props: {
+        id: String,
         title: String,
         description: String
     }
 }
 </script>
+
+<style scoped>
+    .card {
+        margin-bottom: 1em;
+    }
+</style>
 
