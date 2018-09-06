e762abf8dc4a3b083dbfb50cc253d7748024bb00
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-06 08:24:42 -0500

    Created the card component

---
+++ front/src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+        <div class="card-heade">
+            <div class="card-header-title">{{ title }}</div>
+        </div>
+        <div class="card-content">
+            {{ description }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        title: String,
+        description: String
+    }
+}
+</script>
+
