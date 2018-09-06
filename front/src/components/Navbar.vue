f507dffff1cd4548882a743524bb80047d202f17
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-06 11:37:14 -0500

    Link to the home page in title

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -1,9 +1,18 @@
 <template>
   <nav class="navbar is-link">
     <div class="navbar-brand">
-      <h1 class="title is-1 has-text-white">
-        Comm
-      </h1>
+      <router-link to="/">
+        <h1 class="title is-1 has-text-white">
+          Comm
+        </h1>
+      </router-link>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <router-link to="/add-community" class="has-text-white navbar-item">
+          Agregar comunidad
+        </router-link>
+      </div>
     </div>
   </nav>
 </template>
