2c090e506a2f81bc63d8d0255af40a8fba3d84a9
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-06 08:25:12 -0500

    Views folder

---
+++ front/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1 class="title is-1 has-text-center">
+            Hola
+        </h1>
+        <Card title="asdcadcadscac" description="asdchjsbdchjbasdcbhjasdbchjbdhjcdchb"></Card>
+    </div>
+</template>
+
+<script>
+import Card from '@/components/Card'
+
+export default {
+    name: 'Home',
+    components: { Card }
+}
+</script>
+
