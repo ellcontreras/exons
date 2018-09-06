8f8ee060c0dd2bf472c758ab0d256618d876d61d
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-06 11:35:10 -0500

    New headers

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ const communityRouter = require('./routes/community');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next,) => {
+    res.setHeader("Access-Control-Allow-Headers", "content-type")
+    res.setHeader("Content-type", "application/json");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    next();
+});
+
 // Route declaration
 app.use('/api/community', communityRouter);
 
