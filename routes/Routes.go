f50841587f085db6fa26133fa86f37bf96da4522
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-13 11:20:54 -0500

    Change the action for some routes

--- routes/Routes.go
+++ routes/Routes.go
@@ -9,8 +9,8 @@ import (
 //InitRoutes the routes for the server
 func InitRoutes(server *echo.Echo) {
 	//User routes
-	server.GET("/api/communnity/get/all", actions.CommunityGetOne)
-	server.POST("/api/community/get/:id", actions.CommunityGetAll)
+	server.GET("/api/community/get/:id", actions.CommunityGetOne)
+	server.GET("/api/communnity/get/all", actions.CommunityGetAll)
 	server.PUT("/api/community/add", actions.CommunityAdd)
 	server.DELETE("/api/community/update", actions.CommunityUpdate)
 }
