7715035fc9cc1cefa7856c42208fe3cc2666e58c
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-27 15:21:28 -0500

    Change the struct model

--- models/Community.go
+++ models/Community.go
@@ -2,42 +2,19 @@ package models
 
 import (
 	"github.com/labstack/echo"
+
+	"gopkg.in/mgo.v2/bson"
+	"comm/utils"
 )
 
 type Community struct {
-	uid string `json:'uid'; bson:object_id`
-	title string `json:'title'`
-	description string `json:'description'`
-}
-
-//Getters
-
-func (c *Community) GetUID() string {
-	return c.uid
-}
-
-func (c *Community) GetTitle() string {
-	return c.title
-}
-
-func (c *Community) GetDescription() string {
-	return c.description
-}
-
-// Setters
-
-func (c *Community) SetUID(uid string) {
-	c.uid = uid
-}
-
-func (c *Community) SetTitle(title string) {
-	c.title = title
-}
-
-func (c *Community) SetDescription(description string) {
-	c.description = description
+	ID bson.ObjectId `json:"_id" bson:"_id,omitempty"`
+	Title string `json:"title"`
+	Description string `json:"description"`
 }
 
 func (c *Community) BindWithContext(ctx echo.Context) {
-	ctx.Bind(c)
+	err := ctx.Bind(c)
+
+	utils.CheckErr(err)
 }
