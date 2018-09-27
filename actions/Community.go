97768c17a579cba538ea4f2e6a939456ebe67da1
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-27 15:48:24 -0500

    Update community.go, the methods to add and get work!

--- actions/Community.go
+++ actions/Community.go
@@ -1,26 +1,74 @@
 package actions
 
 import (
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+
 	"net/http"
 	"github.com/labstack/echo"
+
+	"comm/utils"
+	"comm/models"
 )
+
+var (
+	err error
+	session *mgo.Session
+	collection *mgo.Collection
+)
+
+func Connect() {
+	session, err = mgo.Dial("mongodb://localhost:27017/comm")
+	utils.CheckErr(err)
+
+	collection = session.DB("comm").C("communities")
+}
+
+func Disconect() {
+	session.Close()
+}
 
 //CommunityGet ...
 func CommunityGetOne(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, "Get Method")
+	community := models.Community{}
+
+	Connect()
+
+	collection.FindId(bson.ObjectIdHex(ctx.Param("id"))).One(&community)
+
+	Disconect()
+	return ctx.JSON(http.StatusOK, community)
 }
 
 func CommunityGetAll(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, "Get all communities")
+	var communities []models.Community
+	
+	Connect()
+
+	err := collection.Find(nil).Sort("-_id").All(&communities)
+	utils.CheckErr(err)
+
+	Disconect()
+	return ctx.JSON(http.StatusOK, communities)
 }
 
 //CommunityPost ...
 func CommunityAdd(ctx echo.Context) error {
-	return ctx.JSON(http.StatusOK, "Post Method")
+	community := models.Community{}
+
+	community.BindWithContext(ctx)
+
+	Connect()
+
+	err = collection.Insert(community)
+	utils.CheckErr(err)
+	
+	Disconect()
+
+	return ctx.JSON(http.StatusOK, community)
 }
 
 //CommunityPut ...
 func CommunityUpdate(ctx echo.Context) error {
 	return ctx.JSON(http.StatusOK, "Put Method")
 }
-
