911d1cd57d06ff374dd88efae52b5f09a04ca1ab
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-28 17:49:21 -0500

    Add the button and handle to delete a community

--- front/src/views/Community.vue
+++ front/src/views/Community.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <div class="notification is-danger" v-if="error">
+            {{ error }}            
+        </div>
         <h1 class="title is-1 has-text-centered">
             {{ community.title }}
         </h1>
@@ -7,6 +10,7 @@
             {{ community.description }}
         </p>
         <router-link :to="`${this.community._id}/update`">Actualizar</router-link>
+        <button @click="clickDelete()" class="button is-danger">Borrar</button>
     </div>
 </template>
 
@@ -17,7 +21,8 @@ export default {
     name: 'Community',
     data() {
         return {
-            community: []
+            community: [],
+            error: ""
         }
     },
     beforeMount() {
@@ -26,6 +31,18 @@ export default {
         }).catch(err => {
             console.log(err);
         })
+    },
+    methods: {
+        clickDelete() {
+            axios.delete("http://localhost:8080/api/community/delete", {data: {
+                _id: this.$route.params.id
+            }}).then(res => {
+                this.$router.push('/');
+            }).catch(error => {
+                console.log(error);
+                this.error = "Ocurrió un error al tratar de eliminar esta comunidad";
+            });
+        }
     }
 }
 </script>
