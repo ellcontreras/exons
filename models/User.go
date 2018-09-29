48d4a810d5f61e471cfc3055bf103f12dfdd2a3e
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-29 13:18:48 -0500

    Created the model User

---
+++ models/User.go
@@ -0,0 +1,22 @@
+package models
+
+import (
+	"github.com/labstack/echo"
+
+	"gopkg.in/mgo.v2/bson"
+	"comm/utils"
+)
+
+type User struct {
+	ID bson.ObjectId `json:"_id" bson:"_id,omitempty"`
+	Name string	`json:"name"`
+	Username string `json:"username"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+func (u *User) BindWithContext(ctx echo.Context) {
+	err := ctx.Bind(u)
+
+	utils.CheckErr(err)
+}
