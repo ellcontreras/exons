289919597f66dfe3cf319dbf45cad346f92d7796
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-29 12:53:35 -0500

    Update utils.go
    Add struct JWTCustomClaims to use jwt

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,20 @@
 package utils
 
+import (
+	"github.com/dgrijalva/jwt-go"
+)
+
+// JWTCustomClaims
+type JWTCustomClaims struct {
+	Name  string `json:"name"`
+	Username string   `json:"username"`
+	Email string `json:"email"`
+	jwt.StandardClaims
+}
+
 // CheckErr verify if exists an error in the var err
 func CheckErr(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
-
