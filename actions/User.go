1338ffd4e2e647149c138d558598ab512e489626
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-09-30 14:53:16 -0500

    Update User.go
    The sign-up method function and use collectionUser

--- actions/User.go
+++ actions/User.go
@@ -2,22 +2,35 @@ package actions
 
 import (
 	"net/http"
-	"github.com/labstack/echo"
 
-	"comm/utils"
+	"github.com/labstack/echo"
+	"gopkg.in/mgo.v2/bson"
+
 	"comm/models"
+	"comm/utils"
 )
 
-func AddUser(ctx echo.Context) error {
+// UserAdd ...
+func UserAdd(ctx echo.Context) error {
 	user := models.User{}
+	userEmail := models.User{}
+	userUsername := models.User{}
 
 	user.BindWithContext(ctx)
 
 	Connect()
 
-	err = collection.Insert(user)
+	collectionUsers.Find(bson.M{"email": user.Email}).One(&userEmail)
+
+	collectionUsers.Find(bson.M{"username": user.Username}).One(&userUsername)
+
+	if len(userEmail.Email) > 0 || len(userUsername.Username) > 0 {
+		return ctx.NoContent(http.StatusConflict)
+	}
+
+	err = collectionUsers.Insert(user)
 	utils.CheckErr(err)
-	
+
 	Disconect()
 
 	return ctx.JSON(http.StatusCreated, user)
