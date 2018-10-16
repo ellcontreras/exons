32ee94ae8b2f02daf354ba46055426335d52f54f
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-10-16 11:43:23 -0500

    Change the name from exons to community

--- front/src/router.js
+++ front/src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Addexonsunity from './views/Addexonsunity.vue'
-import Updateexonsunity from './views/Updateexonsunity.vue'
+import AddCommunity from './views/AddCommunity.vue'
+import UpdateCommunity from './views/UpdateCommunity.vue'
 import Community from './views/Community.vue'
 import UserSignUp from './views/UserSignUp.vue'
 import UserLogin from './views/UserLogin.vue'
@@ -20,14 +20,14 @@ export default new Router({
       component: Home
     },
     {
-      path: '/add-Community',
+      path: '/add-community',
       name: 'add Community',
-      component: Addexonsunity
+      component: AddCommunity
     },
     {
-      path: '/Community/:id/update',
+      path: '/community/:id/update',
       name: 'update Community',
-      component: Updateexonsunity
+      component: UpdateCommunity
     },
     {
       path: '/Community/:id',
