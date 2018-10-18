9c24f0d6af9918b5ab784f067140af3f2828c4fd
Luis Contreras Guerra <luisguerra495@gmail.com>
2018-10-18 12:48:20 -0500

    Solve again a merge conflict

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -3,15 +3,9 @@
         <h1 class="title is-1 has-text-centered">
             exonsunities
         </h1>
-<<<<<<< HEAD
-        <Card v-for="Community in exonsunities" :key="Community._id" 
+        <Card v-for="Community in communities" :key="Community._id" 
             :id="Community._id"
             :title="Community.title" :description="Community.description"/>
-=======
-        <Card v-for="exonsunity in exonsunities" :key="exonsunity._id" 
-            :id="exonsunity._id"
-            :title="exonsunity.title" :description="exonsunity.description"/>
->>>>>>> 7ffc66964085de8bd601e8428984ada54100d2de
     </div>
 </template>
 
@@ -24,12 +18,12 @@ export default {
     components: { Card },
     data() {
         return {
-            exonsunities: []
+            communities: []
         }
     },
     beforeMount() {
         axios.get('http://localhost:8080/api/community/get/all').then(res => {
-            this.exonsunities = res.data;
+            this.communities = res.data;
         }).catch(err => {
             console.log(err);
         });
