1aa77d8afb1b494dc81792d7e2b464e2cf3f737c
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-10-20 18:33:27 -0500

    Change title on the page and display communities in pagination closes#18

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
         <h1 class="title is-1 has-text-centered">
-            exonsunities
+            Comunidades
         </h1>
-        <Card v-for="Community in communities" :key="Community._id" 
-            :id="Community._id"
-            :title="Community.title" :description="Community.description"/>
+
+        <hr>
+
+        <div class="columns" v-for="community in communities" :key="community._id">
+            <div v-for="c in community" :key="c._id" class="column">
+                <Card :id="c._id" :title="c.title" 
+                    :description="c.description"/>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -24,8 +30,15 @@ export default {
     beforeMount() {
         axios.get('http://localhost:8080/api/community/get/all').then(res => {
             this.communities = res.data;
+
+            let arrays = [], size = 3;
+
+            while (this.communities.length > 0)
+                arrays.push(this.communities.splice(0, size));
+
+            this.communities = arrays;
         }).catch(err => {
-            console.log(err);
+            console.log(err.response);
         });
     }
 }
