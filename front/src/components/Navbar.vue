c234fdb54120a2b1ae55f592933b147f71aa6fce
Luis Fernando Contreras Guerra <luisguerra495@gmail.com>
2018-10-23 18:02:05 -0500

    #23 display menu based on session

--- front/src/components/Navbar.vue
+++ front/src/components/Navbar.vue
@@ -7,17 +7,27 @@
         </h2>
       </router-link>
     </div>
-    <div class="navbar-menu">
+    <div class="navbar-menu" v-if="!user">
       <div class="navbar-end">
-        <router-link to="/add-Community" class="has-text-white navbar-item">
-          Agregar comunidad
-        </router-link>
         <router-link to="/sign-up" class="has-text-white navbar-item">
           Registrarme
         </router-link>
         <router-link to="/login" class="has-text-white navbar-item">
           Iniciar Sesión
         </router-link>
+      </div>
+    </div>
+    <div class="navbar-menu" v-if="user">
+      <div class="navbar-end">
+        <router-link to="#" class="has-text-white navbar-item">
+          Hola, {{ user.name }}!
+        </router-link>
+        <router-link to="/add-Community" class="has-text-white navbar-item">
+          Agregar comunidad
+        </router-link>
+        <span @click="handleLogout()" class="has-text-white navbar-item">
+          Cerrar Sesión
+        </span>
       </div>
     </div>
   </nav>
@@ -28,6 +38,23 @@ export default {
   name: 'Navbar',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      user: null
+    }
+  },
+  beforeMount() {
+    if (localStorage.getItem("user")) {
+      this.user = JSON.parse(localStorage.getItem("user")).user;
+    }
+  },
+  methods: {
+    handleLogout() {
+      localStorage.removeItem("user");
+
+      window.location.href = '/';
+    }
   }
 }
 </script>
